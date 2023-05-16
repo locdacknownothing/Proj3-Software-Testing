64062e1d0cbbc2df897f04c62f7d4e136c9ddaaa
phamanhthien1 <62327329+phamanhthien1@users.noreply.github.com>
2023-05-16 15:07:52 +0700

    update name

--- phamanhthien/unit-test/login.py
+++ phamanhthien/unit-test/login.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+# py -m unittest login.py
+
 
 class TestPageLogin(unittest.TestCase):
     def setUp(self):
